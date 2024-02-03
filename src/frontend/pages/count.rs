2d2e0abc96340d67876103dd9d462d1f3470fa39
valentimarco <gamespy67@gmail.com>
2024-02-03 16:02:33 +0100

    Count page

--- src/frontend/pages/count.rs
+++ src/frontend/pages/count.rs
@@ -2,17 +2,17 @@ use askama::Template;
 
 
 #[derive(Template)]
-#[template(path="index.html")]
-pub struct IndexTemplate {
+#[template(path="count.html")]
+pub struct CountTemplate {
     count: i32
 }
 
 static mut COUNT: i32 = 0;
 
 
-pub async fn render_index() -> IndexTemplate {
+pub async fn render_count() -> CountTemplate {
     unsafe { COUNT += 1 };
-    return IndexTemplate {
+    return CountTemplate {
         count: unsafe { COUNT }
     }
 }
--- src/frontend/route.rs
+++ src/frontend/route.rs
@@ -1,8 +1,10 @@
-use axum::{routing::get, Router};
+use axum::{routing::{get, post}, Router};
 
 
-use super::pages::index::render_index;
+use super::pages::{count::render_count, index::render_index};
 
 pub fn route_frontend() -> Router {
-    Router::new().route("/", get(render_index))
+    Router::new()
+    .route("/", get(render_index))
+    .route("/count", post(render_count))
 }
